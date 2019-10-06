8658746c74d5bdcee6decafe015066e8b93dc757
Matthew Ward <matt.ward92@gmail.com>
2019-10-06 18:31:55 +0100

    New UI Tweaks

--- FF1Router/MainWindowRefactor.xaml.cs
+++ FF1Router/MainWindowRefactor.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using KiiLibrary.WPF;
-using MahApps.Metro.Controls;
 
 namespace FF1Router
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindowRefactor.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindowRefactor : Window
     {
-        public MainWindow()
+        public MainWindowRefactor()
         {
             InitializeComponent();
-
-
-            StepsGrid.ConfigureSingleClickEditing();
-            ResultsGrid.DisableSelection();
         }
     }
 }
